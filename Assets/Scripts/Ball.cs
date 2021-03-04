5cc91e124009b972614e96587800e01c48392ee8
Zirlis <Zirlis@yandex.ru>
2021-03-05 02:14:22 +0700

    lesson 1

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.0f;
+    private Rigidbody _rigidbody;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rigidbody.AddForce(-movement * _speed);
+    }
+}
---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private Ball _ball;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position - _ball.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = _ball.transform.position + _offset;
+    }
+}
