7d892da995b441c942be1176e118d5be28f53727
Zirlis <Zirlis@yandex.ru>
2021-03-16 00:31:03 +0700

    add points

---
+++ Assets/Scripts/BonusBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusBall : MonoBehaviour
+{
+    [SerializeField] private GameObject _puzzleWall;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(_puzzleWall);
+            Destroy(gameObject);
+        }
+    }
+}
